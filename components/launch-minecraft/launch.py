57b8330cd79b6bfa2ab1770a94e85f528e614fad
SteefCoder <stefanvanderduijs@icloud.com>
2026-06-07 10:35:59 +0200

    Add luanch

--- components/launch-minecraft/launch.py
+++ components/launch-minecraft/launch.py
@@ -4,7 +4,9 @@ import time
 import qrcode
 import json
 import pathlib
-
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from components.mail.mail import Sender, Browser
 
 def generate_qr(text):
     qr = qrcode.QRCode(border=0)
@@ -16,16 +18,24 @@ def generate_qr(text):
     with open("components/mcserver/world/scripts/shared/qr.json", "w") as f:
         json.dump(matrix, f)
 
-generate_qr("http://172.16.0.47?text=" + "Hello, World")
+
+#name = open("received.txt").read().strip()
+name = sys.argv[1]
+generate_qr("http://172.16.0.47?text=" + name)
 # generate_qr("abc")
 os.system('wmctrl -a "Minecraft* 1.21.11"')
-time.sleep(0.3)
-pg.mouseDown(447, 239)
+pg.sleep(1)
+pg.click(950, 600)
+pg.sleep(1)
+pg.click(447, 239)
 
-time.sleep(3)
+pg.sleep(3)
 
+pg.hotkey("f1")
+pg.sleep(0.2)
 pg.typewrite("t")
-time.sleep(0.2)
+pg.sleep(0.2)
+pg.hotkey("backspace")
 pg.typewrite("/player bob spawn")
 pg.hotkey("enter")
 
@@ -33,23 +43,24 @@ pg.hotkey("enter")
 #pg.typewrite("/fill 20 -61 81 50 -61 121 minecraft:grass_block")
 #pg.hotkey("enter")
 
-time.sleep(1)
+pg.sleep(1)
 
 pg.typewrite("t")
-time.sleep(1)
+pg.sleep(0.5)
+pg.hotkey("backspace")
 pg.typewrite("/script in qrcode invoke create_qr")
 pg.hotkey("enter")
 
-
-pg.hotkey("f1")
 
 p =pathlib.Path("components/mcserver/world/scripts/shared/baz.txt")
 while not p.exists():
     time.sleep(0.3)
 
-p.unlink()
+os.system("rm components/mcserver/world/scripts/shared/baz.txt")
+time.sleep(0.2)
+os.system("scrot -e 'xclip -selection clipboard -t image/png -i $f'")
 
-im = pg.screenshot()
-im.save('components/launch-minecraft/im.png')
+time.sleep(0.1)
 
-pg.hotkey("alt", "tab")
+s = Sender(Browser())
+s.send("lianbitterbal@gmail.com", None)
